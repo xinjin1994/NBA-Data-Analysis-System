8972e7b310bd55169a2819ab2a5fe61f9164af0c
m2jean <n2jeam@sina.com>
2015-03-21 12:06:51 +0800

    player statistic finished
    waiting for test

--- Software/src/main/java/gui/statistic/PlayerStatisticPanel.java
+++ Software/src/main/java/gui/statistic/PlayerStatisticPanel.java
@@ -3,9 +3,7 @@ package gui.statistic;
 import enums.Conference;
 import enums.Division;
 import enums.Position;
-import exceptions.MatchNotFound;
 import exceptions.PlayerNotFound;
-import exceptions.TeamNotFound;
 import gui.SelfAdjustPanel;
 import gui.player.PlayerSearch;
 import gui.player.SearchPlayerPanel;
@@ -13,12 +11,16 @@ import gui.util.ReturnButton;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
+import javax.swing.ButtonGroup;
 import javax.swing.DefaultRowSorter;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
@@ -39,6 +41,7 @@ public class PlayerStatisticPanel extends SelfAdjustPanel implements PlayerSearc
 	private ArrayList<PlayerAdvancedStatsVO> advancedList_total;
 	private JTable tbl_advancedList;
 	private JTable tbl_basicList;
+	private ButtonGroup btngrp;
 
 	public PlayerStatisticPanel() {
 		GridBagLayout gbl_pnl_menu = new GridBagLayout();
@@ -98,11 +101,23 @@ public class PlayerStatisticPanel extends SelfAdjustPanel implements PlayerSearc
 		gbc_pnl_search.fill = GridBagConstraints.HORIZONTAL;
 		add(pnl_search, gbc_pnl_search);
 		
+		JRadioButton rdibtn_average = new JRadioButton("平均");
+		rdibtn_average.setActionCommand("AVERAGE");
+		rdibtn_average.addActionListener(new RadioButtonListener());
+		JRadioButton rdibtn_total = new JRadioButton("总计");
+		rdibtn_total.setActionCommand("TOTAL");
+		rdibtn_total.addActionListener(new RadioButtonListener());
+		btngrp = new ButtonGroup();
+		btngrp.add(rdibtn_average);
+		btngrp.add(rdibtn_total);
+		rdibtn_average.setSelected(true);
+		
 		JPanel pnl_selection = new JPanel();
 		GridBagConstraints gbc_pnl_selection = new GridBagConstraints();
 		gbc_pnl_selection.gridx = 1;
 		gbc_pnl_selection.gridy = 2;
-		//TODO
+		pnl_selection.add(rdibtn_average);
+		pnl_selection.add(rdibtn_total);
 		add(pnl_selection, gbc_pnl_selection);
 		
 		ReturnButton btn_return = new ReturnButton();
@@ -111,6 +126,13 @@ public class PlayerStatisticPanel extends SelfAdjustPanel implements PlayerSearc
 		gbc_btn_return.gridy = 4;
 		gbc_btn_return.anchor = GridBagConstraints.SOUTHWEST;
 		add(btn_return,gbc_btn_return);
+	}
+	
+	class RadioButtonListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent ae) {
+			setList();
+		}
 	}
 
 	@Override
@@ -121,22 +143,26 @@ public class PlayerStatisticPanel extends SelfAdjustPanel implements PlayerSearc
 		} catch (PlayerNotFound e) {
 			basicList_average = new ArrayList<PlayerBasicStatsVO>();
 			basicList_total = new ArrayList<PlayerBasicStatsVO>();
-		} catch (TeamNotFound e) {
-			e.printStackTrace();
-		} catch (MatchNotFound e) {
-			e.printStackTrace();
 		}
-		
 		try {
 			advancedList_average = new PlayersBL().getAdvancedPlayersStatsAverage(c,d,p);
 			advancedList_total = new PlayersBL().getAdvancedPlayersStatsTotal(c,d,p);
 		} catch (PlayerNotFound e) {
 			advancedList_average = new ArrayList<PlayerAdvancedStatsVO>();
 			advancedList_total = new ArrayList<PlayerAdvancedStatsVO>();
-		} catch (TeamNotFound e) {
-			e.printStackTrace();
-		} catch (MatchNotFound e) {
-			e.printStackTrace();
+		}
+		setList();
+	}
+	private void setList(){
+		switch(btngrp.getSelection().getActionCommand()){
+		case "AVERAGE":
+			((PlayerTableModel_Basic)tbl_basicList.getModel()).updateData(basicList_average);
+			((PlayerTableModel_Advanced)tbl_advancedList.getModel()).updateData(advancedList_average);
+			break;
+		case "TOTAL":
+			((PlayerTableModel_Basic)tbl_basicList.getModel()).updateData(basicList_total);
+			((PlayerTableModel_Advanced)tbl_advancedList.getModel()).updateData(advancedList_total);
+			break;
 		}
 	}
 	@SuppressWarnings("unchecked")
